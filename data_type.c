78b640c072b427cd8ce07859b73d4beb1e4d9642
Xue Fang <sh19129@bristol.ac.uk>
2023-11-11 17:03:37 +0000

    update data_type

--- data_type.c
+++ data_type.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
+// int a, b;
+const int a = 5;
+const int b = 2;
+
+
 int main(){
+    // a = 5;
+    // b = 2;
 
     float test_float = 3.5;
     double test_double = 4;
 
-    printf("%.2f\n", test_float);
-    printf("%.4lf\n", test_double);//double is 8 byte; sufficient for 8-15 decimal digits
+    // printf("%.2f\n", test_float);
+    // printf("%.4lf\n", test_double);//double is 8 byte; sufficient for 8-15 decimal digits
+
+    float sum = a/b;
+    float trans_sum = (float) a/b;
+
+    printf("not converted answer: %.2f\n", sum);
+    printf("converted answer: %.2f", trans_sum);
+
 
     return 0;
 }
