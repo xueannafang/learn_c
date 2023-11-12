6d863507752ba18af8a0cb6398c31df5e915b453
Xue Fang <sh19129@bristol.ac.uk>
2023-11-12 14:48:33 +0000

    Create array_test.c

---
+++ array_test.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int test_array[] = {1, 1, 2, 3, 5};
+
+//note that the size of the array cannot be changed.
+//we can define the size of the array by doing below:
+
+int fixed_size_array[10];
+
+// int size_array(int *input_array[]){
+//     int size_of_array = sizeof(input_array);
+//     return size_of_array;
+
+// }
+
+int main(){
+    int i = 4;
+    int to_change = 44;
+    printf("The %d th number in this array is: %d. \n", i, test_array[i]);
+    test_array[i] = to_change;
+    printf("The %d th number in this array has been updated as : %d. \n", i, test_array[i]);
+
+    int size_of_first_array = sizeof(test_array);
+    int size_of_second_array = sizeof(fixed_size_array);
+
+    // int size_of_first_array = size_array(&test_array);
+    // int size_of_second_array = size_array(&fixed_size_array);
+
+    printf("Size of: %d\n%d\n", size_of_first_array, size_of_second_array);
+
+
+
+
+    return 0;
+}
