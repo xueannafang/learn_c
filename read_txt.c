77d560cd911f9064a936b645961554ce52f38027
Xue Fang <sh19129@bristol.ac.uk>
2023-11-17 20:09:34 +0000

    updated read_txt
    use do while ch!=EOF

--- read_txt.c
+++ read_txt.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 
     //File pointer variable to store the value returned by fopen
     FILE *fptr;
+    char ch;
 
     //open the file in the read mode
     fptr = fopen(".\\test_input\\file_read_test.txt", "r");
+    // fptr = fopen(".\\test_input\\file_read_test.txt", "w");
 
     //check if the file is opened successfully.
     if(fptr == NULL){
@@ -17,8 +20,19 @@ int main(){
     }
 
     else{
-        printf("File opened. Now it will be closed.");
+        printf("File opened. The content is: \n");
+
+        do{
+            ch = fgetc(fptr);
+            printf("%c", ch);
+
+        } while(ch != EOF);
+        
+        printf("\nThe file will now be closed.\n");
+        
     }
+
+    
 
     fclose(fptr);
 
